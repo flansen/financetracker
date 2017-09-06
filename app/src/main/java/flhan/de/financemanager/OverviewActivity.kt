48b7468327e27d7e9a493e05593acd720df75063
Florian <floh.pg@gmail.com>
2017-09-06 19:02:16 +0200

    init

---
+++ app/src/main/java/flhan/de/financemanager/OverviewActivity.kt
@@ -0,0 +1,12 @@
+package flhan.de.financemanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+}
