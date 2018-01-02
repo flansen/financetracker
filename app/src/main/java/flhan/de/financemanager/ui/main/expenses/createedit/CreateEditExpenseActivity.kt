a1ecdc8b25931fe1fdb93aebb448d975c4e08faf
Florian Hansen <hansen@adesso-mobile.de>
2018-01-02 09:37:17 +0100

    master: adds example implementation for amount input

--- app/src/main/java/flhan/de/financemanager/ui/main/expenses/createedit/CreateEditExpenseActivity.kt
+++ app/src/main/java/flhan/de/financemanager/ui/main/expenses/createedit/CreateEditExpenseActivity.kt
@@ -160,3 +160,92 @@ class CreateEditExpenseActivity : BaseActivity() {
         }
     }
 }
+
+/*
+<FrameLayout
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    android:layout_marginTop="100dp">
+
+    <TextView
+        android:id="@+id/tv"
+        style="@style/Base.TextAppearance.AppCompat.Display1"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:background="@android:color/transparent"
+        android:clickable="false"
+        android:focusable="false"
+        android:focusableInTouchMode="false"
+        android:foreground="@android:color/transparent"
+        android:gravity="center_horizontal" />
+
+    <EditText
+        android:id="@+id/et"
+        android:layout_width="match_parent"
+        android:layout_height="0dp"
+        android:inputType="number" />
+</FrameLayout>
+
+
+EditText et;
+TextView tv;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_starter);
+
+    et = (EditText) findViewById(R.id.et);
+    tv = (TextView) findViewById(R.id.tv);
+
+    tv.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(final View v) {
+            et.requestFocusFromTouch();
+            InputMethodManager lManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            lManager.showSoftInput(et, SHOW_IMPLICIT);
+        }
+    });
+
+    et.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(final Editable s) {
+            formatInput(s.toString());
+        }
+    });
+
+    formatInput("");
+}
+
+private void formatInput(final String input) {
+    String output;
+
+    if (input.length() == 0) {
+        output = "0.00";
+    } else if (input.length() == 1) {
+        output = "0.0" + input;
+    } else if (input.length() == 2) {
+        output = "0." + input;
+    } else {
+        final String beforeDecSep = input.substring(0, input.length() - 2);
+        final String afterDecSeparator = input.substring(input.length() - 2, input.length());
+        output = String.format("%s.%s", beforeDecSep, afterDecSeparator);
+    }
+
+    final DecimalFormat format = (DecimalFormat) DecimalFormat.getInstance();
+    format.applyPattern("###,##0.00");
+    final String moneyAmount = format.format(Double.parseDouble(output));
+    final String currencySymbol = format.getCurrency().getSymbol();
+    final String moneyString = String.format("%s %s", moneyAmount, currencySymbol);
+    tv.setText(moneyString);
+}*/
