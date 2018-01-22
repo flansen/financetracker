4ad608e2f964d4773823bba2a1bbb71eb6ed6598
Florian <floh.pg@gmail.com>
2018-01-22 20:03:09 +0100

    adds extension for applyWhiteStyle removes custom adapter implementation

--- app/src/main/java/flhan/de/financemanager/common/extensions/ViewExtensions.kt
+++ app/src/main/java/flhan/de/financemanager/common/extensions/ViewExtensions.kt
@@ -1,11 +1,13 @@
 package flhan.de.financemanager.common.extensions
 
 import android.support.annotation.LayoutRes
+import android.support.v7.app.ActionBar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import flhan.de.financemanager.R
 
 /**
  * Created by Florian on 03.10.2017.
@@ -15,6 +17,10 @@ fun View.visible(isVisible: Boolean) {
         this.visibility = VISIBLE
     else
         this.visibility = GONE
+}
+
+fun ActionBar.applyWhiteStyle() {
+    themedContext?.theme?.applyStyle(R.style.white_control_toolbar, true)
 }
 
 infix fun ViewGroup?.inflate(@LayoutRes layoutResourceId: Int): View? {
